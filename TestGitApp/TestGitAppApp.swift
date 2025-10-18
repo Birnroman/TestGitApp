f8d3fe5e6e317950909a7aa4db5e50468b6c6cd3
Birnroman <rgersiev@gmail.com>
2025-10-18 10:33:09 +0300

    Initial commit: TestGitApp Xcode project

---
+++ TestGitApp/TestGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitAppApp.swift
+//  TestGitApp
+//
+//  Created by Rum Gersy on 18.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
